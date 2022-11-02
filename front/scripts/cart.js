fa4474fc3af2ca4207985d67000691edc6fe5278
Lylian Desquilbet <lylian.desquilbet@gmail.com>
2022-11-02 17:40:14 +0100

    first commit

---
+++ front/scripts/cart.js
@@ -0,0 +1,104 @@
+fetch('http://localhost:3000/api/products/')
+.then((response) => response.json())
+.then((data) => {
+  console.log(data);
+
+  const host = "http://localhost:3000/";
+
+
+  let cart = JSON.parse(localStorage.getItem("Products"));
+  console.log(cart)
+
+ 
+  
+  for (i = 0; i < cart.length; i++) {
+  let productSection = document.getElementById("cart__items");
+ 
+
+      cart.forEach(element => {
+        element.forEach(value=>{console.log(value.name)
+        const objectURL = host + "api/products/" + value.productID;
+        console.log(objectURL);  
+
+        fetch(objectURL)
+
+        .then((response) => response.json())
+        .then((dataProduct) => {
+          console.log(dataProduct);
+
+        const productsCart = `
+        <article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+        <div class="cart__item__img">
+          <img src="${value.image}"
+          alt="${value.altTxt}">
+        </div>
+        <div class="cart__item__content">
+          <div class="cart__item__content__description">
+            <h2>${value.name}</h2>
+            <p>${value.choixOpt}</p>
+            <p>${dataProduct.price} €</p>
+          </div>
+          <div class="cart__item__content__settings">
+            <div class="cart__item__content__settings__quantity">
+              <p>Qté : </p>
+              <input index="${[i]}" onchange="updateQty(this)" id="cartQty" type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${value.qty}>
+            </div>
+            <div class="cart__item__content__settings__delete">
+              <p index="${[i]}" id=deleteItems class="deleteItem">Supprimer</p>
+            </div>
+          </div>
+        </div>
+      </article>
+        `;
+        productSection.innerHTML += productsCart;
+        })})
+
+         // Supression de la ligne  au clic du bouton suppr
+         let deleteItems = document.getElementById("deleteItems");
+         deleteItems.addEventListener("click", (event) =>{
+          event.preventDefault();
+          
+          
+      
+      } );
+
+          function deleteItem(e) {
+            let index = e.getAttribute("index");
+            cart.splice(index, 1);
+            localStorage.setItem("Products", JSON.stringify(cart));
+            location.reload();
+          }
+
+        function updateQty(e) {
+          let index = e.getAttribute("index");
+          let newQty = e.value;
+          panier[index][0].qty = newQty;
+      
+          if (newQty == 0) {
+              panier.splice(index, 1);
+              localStorage.setItem("Products", JSON.stringify(cart));
+              location.reload();
+          } else {
+              document.querySelector("#totalQuantity").innerHTML = totalQty();
+              document.querySelector("#totalPrice").innerHTML = totalPrice();
+              localStorage.setItem("Products", JSON.stringify(cart));
+          }
+      }
+
+      function totalPrice() {
+        let totalprix = 0;
+        for (let i = 0; i < cart.length; i++) {
+            let quantity = parseInt(cart[i][0].qty);
+            let prix = parseInt(cart[i][0].price);
+            totalprix += prix * quantity;
+        }
+        return totalprix;
+    }
+
+
+        
+      });
+        
+      }
+    });
+;
---
+++ front/scripts/product.js
@@ -0,0 +1,124 @@
+const queryString = window.location.search;
+console.log(queryString);
+
+const urlParams = new URLSearchParams(queryString);
+
+const id = urlParams.get('id')
+console.log(id);
+
+const host = "http://localhost:3000/";
+const objectURL = host + "api/products/" + id;
+console.log(objectURL);
+
+function createOptionElement (value, text = value) {
+    const option = document.createElement('option')
+  
+    option.innerText = text
+    option.value = value
+  
+    return option
+  }
+
+ 
+
+fetch(objectURL)
+
+.then((response) => response.json())
+.then((data) => {
+  console.log(data);
+
+  let img = document.querySelector(".item__img");
+      img.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
+
+      let name = document.getElementById("title");
+      name.innerHTML = data.name;
+
+      let price = document.getElementById("price");
+      price.innerHTML = data.price;
+
+      let description = document.getElementById("description");
+      description.innerHTML = data.description;
+
+      let color = document.getElementById("colors");
+
+      for (i = 0; i < data.colors.length; i++) {
+        color.innerHTML +=`
+        <option value="${data.colors[i]}">${data.colors[i]}</option>
+        `;
+        
+      }
+      let select = document.querySelector("#colors");
+
+      let addToCart = document.getElementById("addToCart");
+
+      const array = document.querySelectorAll('#colors option');
+        const timeZoneList = document.getElementById('colors');
+        timeZoneList.addEventListener('click', (event) => {
+        let currentArea = event.target.value;
+        for ( let i =0; i<array.length; i++ ) {
+        let currentOption = array[i];
+        if ( currentArea == currentOption.value ) {
+        console.log(currentOption);
+        }
+    }
+});
+
+
+      addToCart.addEventListener("click", (event) =>{
+        event.preventDefault();
+          
+        let image = document.querySelector("body > main > div > section > article > div.item__img > img").src;
+        let imageAlt = document.querySelector("body > main > div > section > article > div.item__img > img").alt;
+        let name = document.getElementById("title").textContent;
+        let price = document.getElementById("price").textContent + "€";
+        let choixOpt = document.querySelector("#colors").value;
+        console.log(choixOpt);
+        let productID = id;
+
+        //transformation du type of qty
+        let qty_chiffre = document.querySelector("#quantity").value;
+        let qty = Number(qty_chiffre);
+
+        //pour tester la boucle et l'arreter
+        let boucle = 0;
+
+        // ajout des elt du panier dans un tableau
+        let eltPanier = [{ productID, name, choixOpt, qty, image, imageAlt}];
+
+        //Déclaration au format js de la clé produit stocké dans le local storage
+        let panierToStock = JSON.parse(localStorage.getItem("Products"));
+
+        //Si le localstorage est vide, on créer tableau, on push le panier dedans et on stock dans localStorage
+        if (!panierToStock) {
+            panierToStock = [];
+            panierToStock.push(eltPanier);
+            localStorage.setItem("Products", JSON.stringify(panierToStock));
+        }
+        //Avant de stock dans local storage, on verifie si nom et option sont =, si = alors on incremente qty
+        else {
+            for (let i = 0; i < panierToStock.length; i++) {
+                if (panierToStock[i][0].name === name && panierToStock[i][0].choixOpt === choixOpt) {
+                    panierToStock[i][0].qty += qty;
+                    boucle = 1;
+                }
+            }
+            //Si pas égale, on stop la boucle et on push le panier dans local storage
+            if (boucle == 0) {
+                panierToStock.push(eltPanier);
+            }
+
+            localStorage.setItem("Products", JSON.stringify(panierToStock));
+        }
+
+        if (qty > 1) {
+            alert(`Vous avez ajouté ${qty} articles au panier`);
+        } else if (qty == 1) {
+            alert(`Vous avez ajouté ${qty} article au panier`);
+        }
+          
+      
+      } 
+
+
+      
+      )} )
